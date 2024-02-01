917dae4dd82920188265fb9b05ae0293b8dab216
DAM 1 <DAM 1@DESKTOP-TKRSI9T>
2024-02-01 10:52:10 +0100

    Estructura del Programa de FranGV

--- PracticaCV_FranGV/Program.cs
+++ PracticaCV_FranGV/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // CONSTANTES
+
+            // RECURSOS
+
+            // ENTRADA
+
+            // PROCESO
+
+            // SALIDA
         }
     }
 }
